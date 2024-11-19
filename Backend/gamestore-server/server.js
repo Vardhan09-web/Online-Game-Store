9428644a24e791f969c9e3ebf28d973ab280ac17
ankamvinayv <ankamvinayv@gmail.com>
2024-11-19 11:16:44 +0530

    gamestore-server code

--- Backend/gamestore-server/server.js
+++ Backend/gamestore-server/server.js
@@ -1,13 +1,28 @@
 const express = require('express')
+const cors = require('cors')
+
+const connectDB = require('./config/db')
+const Users = require('./routes/UserRoute')
+const Games = require('./routes/GameRoute')
+const Orders = require('./routes/OrderRoute')
+const Reviews = require('./routes/ReviewRoute')
+const Auth = require('./routes/AuthRoute')
 const Port = 5000
-
 const app = express()
-
+app.use(cors())
 app.use(express.json())
+connectDB();
 
+
+app.use('/users',Users)
+app.use('/games',Games)
+app.use('/orders',Orders)
+app.use('/reviews',Reviews)
+app.use('/auth', Auth)
 app.get('/', (req, res) => {
-    res.status(200).json("Hello Game Store");
-})
+    res.status(200).json({message: "welcome"})
+    res.send("Hello all, Welcome to MongoDB!");
+});
 
 
 app.listen(Port, () => {
